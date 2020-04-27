13cb8814e2fea422eea3cd7fe948e65db305ba3b
Adewole Adewumi <wolejr@gmail.com>
2020-04-26 22:32:43 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faList, faTh, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-real-estate';
+  faList = faList;
+  faTh = faTh;
+  faMapMarkerAlt = faMapMarkerAlt;
 }
